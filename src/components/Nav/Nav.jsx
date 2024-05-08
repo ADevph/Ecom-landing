6cb2e5ece2c9e5e42c6eaa9f251d94905144f95c
ADevph <das.atanu42914@gmail.com>
2024-05-09 00:23:05 +0600

    efsd

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className='bg-black flex justify-center items-center'>
+            <p className='text-white'>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+            </p>
+            <a className='text-white font-semibold'>Shop Now!</a>
+        </div>
+    );
+};
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Nav from './components/Nav/Nav.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Nav />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
